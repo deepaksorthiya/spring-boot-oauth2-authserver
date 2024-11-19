f2905a3e3c190b4b6d6fb1b0ea737e892078f70f
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-11-19 22:27:46 +0530

    updated file name and enabled debugging

---
+++ src/main/java/com/example/SpringBoot3AuthorizationServerApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity(debug = true)
+public class SpringBoot3AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3AuthorizationServerApplication.class, args);
+    }
+
+}
